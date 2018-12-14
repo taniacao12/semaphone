7673431de87456a5d53eef46b9ffb0d5e7aadaa2
taniacao12 <tcaosu@stuy.edu>
2018-12-14 05:44:13 -0500

    edit main

--- main.c
+++ main.c
@@ -1,60 +1,62 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <string.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#define KEY 1337
 
-#define STR_LEN 200
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short  *array;
+	struct seminfo  *__buf;
+};
 
-int main() {
-    key_t key;
-    key = ftok("makefile", 'J');
+int main(){
+	char * commands = malloc(256);
+	printf("--------------------------------------------------\n");
+	printf("COMMANDS\n create : -C\n remove : -r\n view :   -v\n");
+	printf("--------------------------------------------------\n");
+	printf("What is your command? > ");
+	scanf("%s", commands);
+	while(1){
+		if(!strcmp("-C", commands)){
+			int shared_mem = shmget(KEY,1024,IPC_CREAT | 0644);
+			char *str = (char*)shmat(shared_mem,0,0);
+			str = malloc(256);
+			printf("write something:\n");
+			scanf("%s",str);
+			free(str);
+			shmdt(shared_mem);
+			int semaphore = semget(KEY,2,IPC_CREAT | 0644);
+			int file = open("file.txt", O_CREAT | O_APPEND | 0644);
+			printf("write to file:\n");
+			scanf("%s",str);
+			write(file, str, sizeof(*str));
+			close(file);
+			break;
+		}
+		else if(!strcmp("-v", commands)){
+			char *str;
+			int seg_id;
+			//printf("What is the segment's ID? > ");
+			//scanf("%d",seg_id);
+			int shared_mem = shmget(KEY,1024,0);
+			str = (char*)shmat(shared_mem,0,0);
+			printf("%s\n",str);
+			shmdt(shared_mem);
+			break;
+		}
+		else{
+			printf("What is your command? > ");
+			free(commands);
+		}
+		scanf("%s",commands);
+	}
 
-    int shmid;
-    char *data;
-    char input[STR_LEN];
-    char new_seg = 1;
-
-    while (1) {
-        shmid= shmget(key, STR_LEN, 0644 | IPC_CREAT);
-        data = shmat(shmid, (void *) 0, 0);
-
-        if (new_seg)
-					printf("----- A NEW SEGMENT HAS BEEN CREATED -----\n");
-        else
-					printf("----- EXITISTING SEGMENT -----\n");
-
-        new_seg = 0;
-        input[0] = 0;
-
-        while (strcmp(input, "y") && strcmp(input, "n")) {
-            printf("Do you want to change the data in the segment? (y/n)\n");
-            fgets(input, STR_LEN, stdin);
-            input[strlen(input) - 1] = 0;
-        }
-
-        if (!strcmp(input, "y")) {
-            printf("What do you want to write to the segment?\n");
-            fgets(data, STR_LEN, stdin);
-            printf("You wrote: %s", data);
-        }
-
-        input[0] = 0;
-
-        while (strcmp(input, "y") && strcmp(input, "n")) {
-            printf("Do you want to delete this segment? (y/n)\n");
-            fgets(input, STR_LEN, stdin);
-            input[strlen(input) - 1] = 0;
-        }
-
-        if (!strcmp(input, "y")) {
-            shmdt(data);
-            shmctl(shmid, IPC_RMID, NULL);
-            new_seg = 1;
-            printf("segment has been deleted\n");
-        }
-    }
-
-    return 0;
+	return 0;
 }
